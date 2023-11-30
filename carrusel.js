ae361d2b227be9155d9aa2af82ecdc15bba0ecc2
SkinnyGuille <142051229+SkinnyGuille@users.noreply.github.com>
2023-11-30 19:16:35 -0300

    Add files via upload

---
+++ carrusel.js
@@ -0,0 +1,25 @@
+var imagenes = ["d1.jpg","d3.jpg","d5.jpg","d6.jpg","d7.jpg"];
+var cont = 0;
+
+function imgAnt () {
+    if ( cont > 0){
+        cont-- ;
+    }    
+    else{
+        cont = imagenes.length-1;
+    }
+    fotoactual();
+}
+function imgSig () {
+    if ( cont < imagenes.length-1){
+        cont++ ;
+    }    
+    else{
+        cont = 0;
+    }
+    fotoactual();
+}
+function fotoactual (){
+    var contimg = document.getElementById ("foto-carrusel");
+    contimg.src = imagenes [cont];
+}
